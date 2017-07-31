ed6f2e5822167c5e7b42155dad70278193f12d91
Jeremiah Diboll <jdiboll@gmail.com>
2017-07-31 16:02:51 -0400

    now with seesion

--- todo.js
+++ todo.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
+const expressSession = require('express-session')
 
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
@@ -9,25 +10,48 @@ app.set('view engine', 'mustache')
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(
+  expressSession({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+  })
+)
 
-const todos = []
-const done = []
+// const todos = []
+// const done = []
 
 app.get('/', (req, res) => {
-  res.render('index', { todos: todos, done: done })
+  const todoList = req.session.todoList || []
+  const data = {
+    notcompleted: todoList.filter(todo => !todo.completed),
+    completed: todoList.filter(todo => todo.completed)
+  }
+  res.render('index', data)
 })
 
 app.post('/add', (req, res) => {
-  todos.push(req.body.todo)
-  console.log(todos)
+  const todoList = req.session.todoList || []
+  todoList.push({ id: todoList.length + 1, completed: false, todo: req.body.todo })
+  req.session.todoList = todoList
   res.redirect('/')
 })
 // this part isn't doing what I want.
 app.post('/done', (req, res) => {
-  done.push(req.body.todo)
-  const indexOfItem = todos.indexOf(req.body.todo)
-  todos.splice(indexOfItem, 1)
-  console.log(done)
+  const todoList = req.session.todoList || []
+  const id = parseInt(req.body.id)
+  const todo = todoList.find(todo => todo.id === id)
+  if (todo) {
+    todo.completed = true
+    todo.when = new Date()
+    req.session.todoList = todoList
+  }
+
+  // done.push(req.body.todo)
+  // const indexOfItem = todos.indexOf(req.body.todo)
+  // todos.splice(indexOfItem, 1)
+  // req.session.todos = todos
+  // console.log(done)
   res.redirect('/')
 })
 
